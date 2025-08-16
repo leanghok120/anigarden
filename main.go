a3dd886379f052c2d00bc2d2bdf52d023201dfdc
leanghok <leanghokoeng5@gmail.com>
2025-08-16 10:30:42 +0700

    use alt screen

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	initDB()
 	defer db.Close()
 
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Fatalf("failed to run anigarden: %v\n", err)
 	}
